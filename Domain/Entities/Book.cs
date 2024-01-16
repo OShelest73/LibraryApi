ad652049c066d20b9aefde03ad2c81bb2a94164b
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2024-01-16 19:09:14 +0300

    init: base structure

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Book
+{
+    int Id { get; set; }
+    string ISBN { get; set; }
+    string Genre { get; set; }
+    string Description { get; set; }
+    string Author { get; set; }
+    DateTime BorrowingTime { get; set; }
+    DateTime ReturnTime { get; set; }
+}
---
+++ LibraryApi/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LibraryApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<>> GetAllBooks()
+    {
+        
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<>> GetBookById(int id)
+    {
+        
+    }
+
+    [HttpGet("isbn/{isbn}")]
+    public async Task<ActionResult<>> GetBookByISBN(string isbn)
+    {
+        
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateBook(Book book)
+    {
+        
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateBook(Book book)
+    {
+        
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteBookById(int id)
+    {
+        
+    }
+
+}
